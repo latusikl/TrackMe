95d27adab14dad7291fb6e220106b66e5eef561d
Łukasz Latusik <latusikl@gmail.com>
2020-11-11 14:40:00 +0100

    Rename fragments, add app name

--- app/src/main/java/pl/latusikl/trackme/AboutFragment.kt
+++ app/src/main/java/pl/latusikl/trackme/AboutFragment.kt
@@ -8,17 +8,14 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class AboutFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
